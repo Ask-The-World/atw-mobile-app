a9f0c6d4d001d2eb48196069e8eb350142661c22
TecTrixer <tonihoevedes@gmail.com>
2020-12-31 17:36:11 +0100

    first test of stack with card widgets

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xff1a1d33),
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -80,30 +81,61 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: Stack(
+          children: [
+            Positioned(
+              top: -20.0,
+              left: 20.0,
+              child: Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    color: Color(0xFF3f80a0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(50),
+                    child: Text(
+                      "Hello, 2nd Test",
+                      style: TextStyle(color: Color(0xffeefdff)),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Draggable(
+              axis: Axis.vertical,
+              childWhenDragging: Container(),
+              feedback: Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    color: Color(0xFF3f80a0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Hello, Test",
+                      style: TextStyle(color: Color(0xffeefdff)),
+                    ),
+                  ),
+                ),
+              ),
+              child: Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2),
+                    color: Color(0xFF3f80a0),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Hello, Test",
+                      style: TextStyle(color: Color(0xffeefdff)),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
