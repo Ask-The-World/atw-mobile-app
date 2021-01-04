58fb11b8a7c0c3d5ccec41a5e9a380a060fb139f
TecTrixer <tonihoevedes@gmail.com>
2021-01-05 00:24:07 +0100

    gets config of server at first start of app

--- lib/backend/getconfig.dart
+++ lib/backend/getconfig.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
-
+import 'storage.dart';
 import 'package:http/http.dart' as http;
 
-class GetQuestion {
+class GetConfig {
   final int minTime;
   final int maxTime;
   final int defaultTime;
   final int maxQuestionLength;
-  GetQuestion(
+  GetConfig(
       {this.minTime, this.maxTime, this.defaultTime, this.maxQuestionLength});
 
-  factory GetQuestion.fromJson(Map<String, dynamic> json) {
-    return GetQuestion(
+  factory GetConfig.fromJson(Map<String, dynamic> json) {
+    return GetConfig(
       minTime: json["min_time"],
       maxTime: json["max_time"],
       defaultTime: json["default_time"],
@@ -40,12 +40,12 @@ class CustomException implements Exception {
   }
 }
 
-Future<GetQuestion> fetchGetQuestion() async {
+Future<GetConfig> fetchGetConfig() async {
   final response = await http.get('http://192.168.1.105/api/get/config');
 
   if (response.statusCode == 200) {
     print('Successfull connection (200)');
-    return GetQuestion.fromJson(jsonDecode(response.body));
+    return GetConfig.fromJson(jsonDecode(response.body));
   } else if (response.statusCode == 500) {
     print("error connection (500)");
     throw CustomException(
@@ -53,4 +53,36 @@ Future<GetQuestion> fetchGetQuestion() async {
   } else
     print("no connection to anything");
   throw Exception("Could not connect to backend");
+}
+
+GetConfig getConfig() {
+  storageGetString("gotConfigVars").then((result) {
+    if (result != "True") {
+      GetConfig config;
+      fetchGetConfig().then((configuration) {
+        config = configuration;
+        storageSetString("gotConfigVars", "True");
+        storageSetInt("minTime", config.minTime);
+        storageSetInt("maxTime", config.maxTime);
+        storageSetInt("defaultTime", config.defaultTime);
+        storageSetInt("maxQuestionLenght", config.maxQuestionLength);
+        return config;
+      }).catchError((error) {
+        storageSetString("gotConfigVars", "False");
+        storageSetInt("minTime", 20);
+        storageSetInt("maxTime", 120);
+        storageSetInt("defaultTime", 60);
+        storageSetInt("maxQuestionLength", 255);
+        print("error getting config from server: ${error}");
+        return GetConfig(
+            defaultTime: 60, minTime: 20, maxTime: 120, maxQuestionLength: 255);
+      });
+    } else {
+      return GetConfig(
+          defaultTime: storageGetInt("defaultTime"),
+          maxQuestionLength: storageGetInt("maxQuestionLength"),
+          minTime: storageGetInt("minTime"),
+          maxTime: storageGetInt("maxTime"));
+    }
+  });
 }
